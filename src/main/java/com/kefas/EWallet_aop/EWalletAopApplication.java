3c8c48ebb9ba671e100ee1571bc3ecff49df5076
Decagon <decagon@Decagons-MacBook-Air.local>
2023-03-02 21:59:57 +0100

    Initial Commit

---
+++ src/main/java/com/kefas/EWallet_aop/EWalletAopApplication.java
@@ -0,0 +1,13 @@
+package com.kefas.EWallet_aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWalletAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWalletAopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kefas/EWallet_aop/EWalletAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kefas.EWallet_aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWalletAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
