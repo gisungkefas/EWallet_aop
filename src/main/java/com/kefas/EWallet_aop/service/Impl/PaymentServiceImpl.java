e022d76c84c39c5c8622c910f088662101b40c60
Decagon <decagon@Decagons-MacBook-Air.local>
2023-03-06 14:25:56 +0100

    complited payment business logic

--- src/main/java/com/kefas/EWallet_aop/service/Impl/PaymentServiceImpl.java
+++ src/main/java/com/kefas/EWallet_aop/service/Impl/PaymentServiceImpl.java
@@ -2,7 +2,10 @@ package com.kefas.EWallet_aop.service.Impl;
 
 import com.kefas.EWallet_aop.entity.User;
 import com.kefas.EWallet_aop.entity.Wallet;
+import com.kefas.EWallet_aop.exception.PaymentException;
 import com.kefas.EWallet_aop.exception.WalletException;
+import com.kefas.EWallet_aop.pojo.paystack.Bank;
+import com.kefas.EWallet_aop.pojo.paystack.TransferRecipient;
 import com.kefas.EWallet_aop.pojo.paystack.request.AccountRequest;
 import com.kefas.EWallet_aop.pojo.paystack.request.CreateCustomerRequest;
 import com.kefas.EWallet_aop.pojo.paystack.request.SetUpTransactionRequest;
@@ -15,13 +18,17 @@ import com.kefas.EWallet_aop.util.AppUtil;
 import com.kefas.EWallet_aop.util.AuthDetails;
 import com.kefas.EWallet_aop.util.LocalStorage;
 import com.kefas.EWallet_aop.util.PayStackHttpEntity;
+import com.mongodb.client.model.geojson.LineString;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.security.Principal;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -44,62 +51,141 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public SetUpTransactionResponse initializeTransaction(SetUpTransactionRequest request) {
-        return null;
+        String url = "https://api.paystack.co/transaction/initialize";
+        return restTemplate.exchange(
+                url, HttpMethod.POST, payStackHttpEntity.getEntity(request),
+                SetUpTransactionResponse.class
+        ).getBody();
     }
 
     @Override
     public VerifyPaymentResponse verifyTransaction(Principal principal, String reference) {
-        return null;
+        checksBeforeTransaction(principal);
+        return verifyTransaction(reference);
     }
 
     @Override
     public VerifyPaymentResponse verifyTransaction(String reference) {
-        return null;
+        String url = "https://api.paystack.co/transaction/verify" + reference;
+        return restTemplate.exchange(
+                url, HttpMethod.GET,payStackHttpEntity.getEntity(),
+                VerifyPaymentResponse.class
+        ).getBody();
     }
 
     @Override
     public TransactionsResponse fetchTransactions(Principal principal) {
-        return null;
+        checksBeforeTransaction(principal);
+
+        String url = "https://api.paystack.co/transaction";
+        return restTemplate.exchange(
+                url, HttpMethod.GET, payStackHttpEntity.getEntity(),
+                TransactionsResponse.class
+        ).getBody();
     }
 
     @Override
     public TransactionResponse fetchTransaction(Principal principal, long id) {
-        return null;
+        authDetails.validateActiveUser(principal);
+
+        String url = "https://api.paystack.co/transaction/" + id;
+        return restTemplate.exchange(
+                url, HttpMethod.GET, payStackHttpEntity.getEntity(),
+                TransactionResponse.class
+        ).getBody();
     }
 
     @Override
     public BankResponse fetchBanks(Principal principal, String currency, String type) {
-        return null;
+        checksBeforeTransaction(principal);
+
+        String url = "https://api.paystack.co/bank?currency" + currency + "&type=" + type;
+        BankResponse bankResponse = restTemplate.exchange(
+                url, HttpMethod.GET, payStackHttpEntity.getEntity(),
+                BankResponse.class
+        ).getBody();
+
+        List<Bank> banks = Objects.requireNonNull(bankResponse).getData().stream().filter(Bank::isActive).toList();
+        bankResponse.setData(banks);
+        return bankResponse;
     }
 
     @Override
     public VerifyAccountResponse verifyAccount(Principal principal, String accountNumber, String bankCode) {
-        return null;
+        checksBeforeTransaction(principal);
+
+        String url = "https://api.paystack.co/bank/resolve?account_number=" + accountNumber + "&bank_code=" + bankCode;
+        return restTemplate.exchange(
+                url, HttpMethod.GET, payStackHttpEntity.getEntity(),
+                VerifyAccountResponse.class
+        ).getBody();
     }
 
     @Override
     public TransferRecipientResponse setUpTransferRecipient(Principal principal, AccountRequest request) {
-        return null;
+        User user = checksBeforeTransaction(principal);
+
+        String url = "https://api.paystack.co/transferrecipient";
+        TransferRecipientResponse transferRecipientResponse = restTemplate.exchange(
+                url, HttpMethod.POST, payStackHttpEntity.getEntity(),
+                TransferRecipientResponse.class
+        ).getBody();
+
+        String reference = util.generateSerialNumber("Tsf") + "Ref";
+        TransferRecipient transferRecipient = Objects.requireNonNull(transferRecipientResponse).getData();
+        transferRecipient.setReference(reference);
+        localStorage.save(TRANSFER_RECIPIENT + user.getEmail(), reference, 3600);
+        transferRecipientResponse.setData(transferRecipient);
+        return transferRecipientResponse;
     }
 
     @Override
     public TransferResponse setUpTransfer(Principal principal, TransferRequest request, String reference) {
-        return null;
+        User user = checksBeforeTransaction(principal);
+
+        String storedReference = localStorage.getValueByKey(TRANSFER_RECIPIENT + user.getEmail());
+        if((reference == null || storedReference == null) ||!reference.equals(storedReference)){
+            throw new PaymentException("Transaction session has expired or reference does not exist. " +
+                    "Kindly initiate a new transfer");
+        }
+        localStorage.clear(TRANSFER_RECIPIENT + user.getEmail());
+
+        String url = "https://api.paystack.co/transfer";
+        return restTemplate.exchange(
+                url, HttpMethod.POST, payStackHttpEntity.getEntity(),
+                TransferResponse.class
+        ).getBody();
     }
 
     @Override
     public VerifyTransferResponse verifyTransfer(Principal principal, String reference) {
-        return null;
+        checksBeforeTransaction(principal);
+
+        String url = "https://api.paystack.co/transfer/verify/" + reference;
+        VerifyTransferResponse response = restTemplate.exchange(
+                url, HttpMethod.GET, payStackHttpEntity.getEntity(),
+                VerifyTransferResponse.class
+        ).getBody();
+
+        return response;
     }
 
     @Override
     public CreateCustomerResponse createCustomer(CreateCustomerRequest request) {
-        return null;
+        String url = "https://pai.paystack.co/customer";
+        return restTemplate.exchange(
+                url, HttpMethod.POST, payStackHttpEntity.getEntity(request),
+                CreateCustomerResponse.class
+        ).getBody();
     }
 
     @Override
     public CustomerValidationResponse validateCustomer(WalletValidationRequest request) {
-        return null;
+        String url = "https://api.paystack.co/customer/" + request.getCustomer_code() + "/identification";
+        return restTemplate.exchange(
+                url, HttpMethod.POST, payStackHttpEntity.getEntity(request),
+                CustomerValidationResponse.class
+        ).getBody();
     }
 
     public User checksBeforeTransaction(Principal principal){
